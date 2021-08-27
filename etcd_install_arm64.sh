aeb51607801933b28dc665fbb07d3154fab556fd
Tran Giang Long <trangianglong4003@gmail.com>
2021-08-27 22:39:22 +0200

    feat(script): fix linux

--- etcd_install_arm64.sh
+++ etcd_install_arm64.sh
@@ -8,7 +8,7 @@ GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
 DOWNLOAD_URL=${GOOGLE_URL}
 
 rm -f /tmp/etcd-${ETCD_VER}-linux-arm64.tar.gz
-rm -rf /tmp/etcd-download-test && mkdir -p /tmp/etcd-download-test
+rm -rf /tmp/test-etcd && mkdir -p /tmp/test-etcd
 
 curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-arm64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-arm64.tar.gz
 tar xzvf /tmp/etcd-${ETCD_VER}-linux-arm64.tar.gz -C /tmp/test-etcd --strip-components=1
