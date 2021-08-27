d8bc8b84e609e08a9b193c24e43706e5f8c3e327
Tran Giang Long <trangianglong4003@gmail.com>
2021-08-27 22:52:06 +0200

    feat(script): fix linux

--- etcd_install_linux.sh
+++ etcd_install_linux.sh
@@ -1,17 +1,19 @@
 #! /bin/sh
 
 ETCD_VER=v3.5.0
-ETCD_BIN=/tmp/test-etcd
 
+# choose either URL
 GOOGLE_URL=https://storage.googleapis.com/etcd
 GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
-
-DOWNLOAD_URL=${GITHUB_URL}
+DOWNLOAD_URL=${GOOGLE_URL}
 
 rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
-rm -rf $ETCD_BIN
-mkdir -p $ETCD_BIN
+rm -rf /tmp/test-etcd && mkdir -p /tmp/test-etcd
 
 curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/test-etcd --strip-components=1
+rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 
-tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C $ETCD_BIN --strip-components=1
+/tmp/test-etcd/etcd --version
+/tmp/test-etcd/etcdctl version
+/tmp/test-etcd/etcdutl version
