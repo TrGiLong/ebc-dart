ca7f0ea7d464d1aeafebfc242bde51110a6ec104
Tran Giang Long <trangianglong4003@gmail.com>
2021-09-15 12:24:06 +0200

    feat(script): improve

---
+++ scripts/process_monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Usage: ./process-usage.sh <PID of the process>
+export PID=$1
+rm top.dat
+while true; do top -p $PID -bMn 1 | egrep '^[0-9]+' | awk -v now=$(date +%s) '{print now,$6,$9}' >> top.dat; done
