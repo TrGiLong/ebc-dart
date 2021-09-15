178abdf74d7f066cfdd7f9c64c1e3dba51523383
Tran Giang Long <trangianglong4003@gmail.com>
2021-09-15 12:54:34 +0200

    fix(script): monitor

--- scripts/process_monitor.sh
+++ scripts/process_monitor.sh
@@ -2,4 +2,4 @@
 # Usage: ./process-usage.sh <PID of the process>
 export PID=$1
 rm top.dat
-while true; do top -p $PID -bMn 1 | egrep '^[0-9]+' | awk -v now=$(date +%s) '{print now,$6,$9}' >> top.dat; done
+while true; do top -p $PID -bn 1 | egrep '^[0-9]+' | awk -v now=$(date +%s) '{print now,$6,$9}' >> top.dat; done
